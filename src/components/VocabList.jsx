4d6916872b4ca1ba8e26d1cdb7867da1a423b28e
= <devchap8@gmail.com>
2026-07-31 09:25:45 -0400

    Clean data

--- src/components/VocabList.jsx
+++ src/components/VocabList.jsx
@@ -5,7 +5,6 @@ import data from "../data/dataIndex";
 export default function VocabList({}) {
     const params = useParams();
     const vocab = data.vocab[params.chapter];
-    console.log(data);
 
     return (
         <table>
@@ -19,7 +18,7 @@ export default function VocabList({}) {
             </thead>
             <tbody>
             {vocab.map(v => 
-                <tr>
+                <tr key={v.def}>
                     <td>{v.reading}</td>
                     <td>{v.kanji}</td>
                     <td>{v.type}</td>
