f989ec8282deae2323032ea3b1ab90caaf909054
= <devchap8@gmail.com>
2026-08-05 15:17:35 -0400

    Make hide definition button

---
+++ src/components/SpoilerText.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react"
+
+export default function SpoilerText(props) {
+    const [hidden, setHidden] = useState(true);
+    const [clicked, setClicked] = useState(false);
+
+    const toggleHidden = () => setHidden(!hidden);
+    const toggleClicked = () => {
+        setClicked(!clicked);
+        setHidden(clicked);
+    }
+
+    return (
+        <div className={hidden ? "spoiler-hidden" : "spoiler-not-hidden"} onClick={toggleClicked} onMouseOver={hidden && !clicked && toggleHidden} onMouseOut={!hidden && !clicked && toggleHidden}>
+            {props.children}
+        </div>
+    )
+}
--- src/components/VocabList.jsx
+++ src/components/VocabList.jsx
@@ -1,5 +1,7 @@
 import { useParams } from "react-router";
+import { useState } from "react";
 import data from "../data/dataIndex";
+import SpoilerText from "./SpoilerText";
 
 const sortVals = {
     "n.": 1, "い-adj.": 2, "な-adj.": 3, "irr-v.": 4, "u-v.": 5, "ru-v.": 6,
@@ -8,11 +10,19 @@ const sortVals = {
 
 export default function VocabList({}) {
     const params = useParams();
+    const [defsHidden, setDefsHidden] = useState(false);
     let vocab = data.vocab[params.chapter].toSorted((a, b) => sortVals[a.type] - sortVals[b.type]);
 
     return (
         <div className="h-full bg-bg-main text-text-main flex flex-col justify-center p-4 gap-6">
+
             <h2 className="text-center text-4xl font-bold">Lesson {params.chapter.replace("L", "")} Vocab</h2>
+
+            <label className="max-w-200 w-full self-center flex gap-3 justify-end items-center" htmlFor="hideDefsCheckbox">
+                <div>Hide Definitions</div>
+                <input className="size-5" onClick={() => setDefsHidden(!defsHidden)} checked={defsHidden} type="checkbox" id="hideDefsCheckbox" name="hideDefsCheckbox"></input>
+            </label>
+            
             <table className="max-w-200 w-full self-center border-2 border-genki-orange">
                 <thead>
                 <tr>
@@ -28,7 +38,12 @@ export default function VocabList({}) {
                         <td className="border border-genki-orange p-1"><a className="underline underline-offset-3" href={`https://jisho.org/search/${v.reading}`} target="_blank" rel="noopener noreferrer">{v.reading}</a></td>
                         <td className="border border-genki-orange p-1">{v.kanji}</td>
                         <td className="border border-genki-orange p-1">{v.type}</td>
-                        <td className="border border-genki-orange p-1">{v.def}</td>
+                        <td className="border border-genki-orange p-1">
+                            {defsHidden
+                                ? <SpoilerText><div className="w-full">{v.def}</div></SpoilerText>
+                                : v.def
+                            }
+                        </td>
                     </tr>
                 )}
                 </tbody>
