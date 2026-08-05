404a84f9d38d958e2e4660bc11b49a64943d548d
= <devchap8@gmail.com>
2026-08-05 13:46:10 -0400

    Vocab list sorts by type

--- src/components/VocabList.jsx
+++ src/components/VocabList.jsx
@@ -1,9 +1,14 @@
 import { useParams } from "react-router";
 import data from "../data/dataIndex";
 
+const sortVals = {
+    "n.": 1, "い-adj.": 2, "な-adj.": 3, "irr-v.": 4, "u-v.": 5, "ru-v.": 6,
+    "adv.": 7, "exp.": 8, "pre.": 9, "suf.": 10, "part.": 11
+}
+
 export default function VocabList({}) {
     const params = useParams();
-    const vocab = data.vocab[params.chapter];
+    let vocab = data.vocab[params.chapter].toSorted((a, b) => sortVals[a.type] - sortVals[b.type]);
 
     return (
         <div className="h-full bg-bg-main text-text-main flex flex-col justify-center p-4 gap-6">
