235e4d68efee535d7e7b15192bf3d76b31dc5c0a
= <devchap8@gmail.com>
2026-08-14 14:35:14 -0400

    Make quiz select responsive

--- src/components/VocabQuiz.jsx
+++ src/components/VocabQuiz.jsx
@@ -23,8 +23,8 @@ export default function VocabQuiz({}) {
                 <div className="text-bg-main">Quiz explanation here: Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quo, mollitia?</div>
             </div>
 
-            {!firstThree.includes(params.chapter) && <div>
-                <div className="inline-block mr-5 text-text-main text-lg">Quiz Type:</div>
+            {!firstThree.includes(params.chapter) && <div className="text-xs sm:text-base">
+                <div className="md:inline-block text-center pb-3 md:pb-0 md:mr-5 text-text-main text-lg">Quiz Type:</div>
                 <a className={`px-3 py-2 ${params.quizType === "match" ?  "bg-genki-orange" : "bg-bg-dim opacity-90"} rounded-l-lg`} href={`/vocabQuiz/${params.chapter}/${params.subsect}/match`}>Match Definition</a>
                 <a className={`px-3 py-2 ${params.quizType === "kana" ? "bg-genki-orange" : "bg-bg-dim opacity-90"}`}              href={`/vocabQuiz/${params.chapter}/${params.subsect}/kana`}>Write Kana</a>
                 <a className={`px-3 py-2 ${params.quizType === "kanji" ? "bg-genki-orange" : "bg-bg-dim opacity-90"} rounded-r-lg`} href={`/vocabQuiz/${params.chapter}/${params.subsect}/kanji`}>Write Kanji</a>
