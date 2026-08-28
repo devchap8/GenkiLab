ce44d1b7436e8639f4c41b62957178319ecbb1d2
= <devchap8@gmail.com>
2026-08-28 18:50:18 -0400

    Import 'in this lesson' descriptions

--- src/data/dataIndex.js
+++ src/data/dataIndex.js
@@ -54,6 +54,33 @@ const lessonNames = {
     L23: "Good-bye"
 };
 
+const inThisLesson = {
+    L0: ["Read hiragana and katakana", "Greet people", "Read numbers and count"],
+    L1: ["Introduce Ourselves", "Ask and tell people names, majors at school, time, etc."],
+    L2: ["Ask and answer how much things cost", "Go shopping", "Order food in a restaurant"],
+    L3: ["Talk about daily activities", "Extend, accept, and refuse invitations"],
+    L4: ["Ask and descripe where things/people are", "Talk about things that happened in the past", "Talk about habitual actions in the past"],
+    L5: ["Talk about travel", "Describe people and things", "Make offers and invitations", "Talk about likes and dislikes"],
+    L6: ["Make requests", "Ask for and give permission", "Talk about rules and regulations", "Offer help", "Give reasons for doing/not doing something"],
+    L7: ["Talk about families and friends", "Describe how people are dressed and how they look"],
+    L8: ["Talk casually", "Express thoughts and opinions", "Report someone's speech", "Request not to do", "Talk about things we like/dislike"],
+    L9: ["Talk casually about the things that happened in the past", "Express thoughts and opinions about past events", "Report someone's speech", "Order food at a restaurant or shop", "Give reasons"],
+    L10: ["Compare things and people", "Talk about future plans", "Describe changes in states", "Talk about means of transportation and the time required", "Ask about tours and make reservations"], 
+    L11: ["Express what we want to do", "Talk about our experiences", "Introduce friends to each other", "Ask and talk about hometowns"],
+    L12: ["Give and ask for an explanation", "Complain about something being too much", "Express what we have to do", "Describe symptoms of illness", "Give advice"],
+    L13: ["Say what we can or cannot do", "Give several reasons", "Express first impressions", "Talk about part-time job experience"],
+    L14: ["Express what we want", "Talk about uncertain things", "Give and receive presents", "Talk about Valentine's Day and special days"],
+    L15: ["Suggest doing something together", "Make preparations", "Describe people or things in detail", "Make plans for the trip with friends"],
+    L16: ["Talk about doing a favor", "Express our hopes and wishes", "Apologize", "Describe lost items"],
+    L17: ["Tell what we hear from others", "Talk about hypothetical situations", "Point out similarities", "Grumble about our situations"],
+    L18: ["Describe the states of things", "Talk about failures", "Express our regret", "Talk with the manager at a workplace"],
+    L19: ["Show our respect", "Express gratitude", "Talk about things we are glad that we did", "Talk politely with bosses"],
+    L20: ["Speak modestly about ourselves", "Express what is difficult or easy to do", "Return and exchange merchandise", "Ask for directions"],
+    L21: ["Talk about bad experiences", "Check if things have been prepared", "Tell somebody what we wish them to do", "Report incidents to the police"],
+    L22: ["Talk about getting people to do things", "Instruct people to do things", "Ask for and give advice", "Express opinions on education"],
+    L23: ["Complain about doing something undesirable", "Make a resolution for a new phase of life", "Reminisce about the past", "Say farewell"]
+}
+
 const subsects = {};
 Object.entries(vocab).forEach(v => {
     const subsect = new Set();
