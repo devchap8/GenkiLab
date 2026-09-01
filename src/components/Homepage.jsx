af8f8cb28aafe64fe1c3a9034da9a8ae58b0c0a4
= <devchap8@gmail.com>
2026-09-01 14:24:56 -0400

    Make homepage para, logos

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -9,6 +9,15 @@ const extraLinks = {
 export default function Homepage() {
     return (
         <div className="bg-bg-main h-full flex flex-col items-center p-4">
+            <section className="max-w-225">
+                <h1 className="text-5xl font-bold text-genki-orange tracking-wide text-center mb-5">Welcome to GenkiLab!</h1>
+                <div className="text-text-main whitespace-pre-line mb-5 flex flex-col gap-2">
+                    <div>GenkiLab is a study resource meant to help people learning Japanese through the Genki I and II (Third Edition) textbooks. Each lesson has its own vocab list and quizzes, with more features coming soon.</div>
+                    <div>GenkiLab is an independent, unofficial fan resource and is not affiliated with The Japan Times, the publisher of the Genki series. All rights to the Genki textbooks belong to their respective owners. This site is meant to be used alongside the official textbooks and workbooks, not as a replacement. Please support the original creators by purchasing the Genki series.</div>
+                    <div>Found a mistake? Have some feedback? Email me at <p className="inline text-blue-400">devchap8@uri.edu</p></div>
+                    <div>Planned features: Hiragana / Katakana quizzes, chapter overviews, grammar explanation resources, grammar quizzes, and more!</div>
+                </div>
+            </section>
             <main className=" max-w-165 w-full p-2 flex flex-col gap-5">
                 <h2 className="text-center text-4xl font-bold text-text-main">Chapter Navigation</h2>
                 {lessonNums.map(n => 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,18 @@
+import logo from '../assets/logos/logo-clear-white.svg';
+
+
 export default function Navbar() {
     return (
         <nav className="bg-genki-orange h-28 min-h-28">
-            <a href="/">Genki Workbook</a>
+            <a href="/">
+                <img 
+                    src={logo} 
+                    alt="GenkiLab Logo"
+                    className='w-30 ml-1'
+                >
+
+                </img>
+            </a>
         </nav>
     )
 }
