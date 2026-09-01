9437784e4d8c52429ece46c18a740c5dcac4ad4a
= <devchap8@gmail.com>
2026-09-01 15:02:05 -0400

    Change header styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,15 +3,17 @@ import logo from '../assets/logos/logo-clear-white.svg';
 
 export default function Navbar() {
     return (
-        <nav className="bg-genki-orange h-28 min-h-28">
-            <a href="/">
-                <img 
-                    src={logo} 
-                    alt="GenkiLab Logo"
-                    className='w-30 ml-1'
-                >
-
-                </img>
+        <nav className="bg-bg-main h-16 min-h-16 flex items-center justify-between px-4">
+            <a href="/" className="inline-block">
+                <div className="flex items-center gap-2">
+                    <img
+                        src={logo}
+                        alt="GenkiLab Logo"
+                        className='w-10'
+                    />
+                    <span className="text-text-main text-xl font-bold tracking-wide">GenkiLab</span>
+                </div>
+                <div className="bg-genki-orange h-0.5 mt-1"></div>
             </a>
         </nav>
     )
