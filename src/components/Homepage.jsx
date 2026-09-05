2b4dd42add931b761b4e4ff16c796302b05c4497
= <devchap8@gmail.com>
2026-09-05 15:42:11 -0400

    Give homepage docTitle

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,4 +1,5 @@
 import ChapterIndex from "./ChapterIndex";
+import useDocTitle from "../scripts/useDocTitle";
 
 const lessonNums = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23];
 const extraLinks = {
@@ -6,6 +7,7 @@ const extraLinks = {
 };
 
 export default function Homepage() {
+    useDocTitle("Home");
     return (
         <div className="bg-bg-main h-full flex flex-col items-center p-4">
             <section className="max-w-225">
